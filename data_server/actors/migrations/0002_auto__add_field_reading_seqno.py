b6a402b75842eb17ca1ed4f64a455c8aaf0c63e9
Andus Lim <yuncai83@gmail.com>
2015-06-05 20:16:06 +0800

    added migration script

---
+++ data_server/actors/migrations/0002_auto__add_field_reading_seqno.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Reading.seqno'
+        db.add_column('actors_reading', 'seqno',
+                      self.gf('django.db.models.fields.PositiveIntegerField')(default=0),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Reading.seqno'
+        db.delete_column('actors_reading', 'seqno')
+
+
+    models = {
+        'actors.configurationsequence': {
+            'Meta': {'object_name': 'ConfigurationSequence'},
+            'gateway_ip': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '128'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ip_address': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '128'}),
+            'node_profile_type': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '1'}),
+            'type_id': ('django.db.models.fields.PositiveSmallIntegerField', [], {})
+        },
+        'actors.deployment': {
+            'Meta': {'object_name': 'Deployment'},
+            'cluster': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '128'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '128'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        'actors.node': {
+            'Meta': {'object_name': 'Node'},
+            'conf_seq': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['actors.ConfigurationSequence']", 'related_name': "'node'"}),
+            'deployment': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['actors.Deployment']", 'related_name': "'node'"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'node_id': ('django.db.models.fields.IntegerField', [], {})
+        },
+        'actors.reading': {
+            'Meta': {'object_name': 'Reading'},
+            'gwtimestamp': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2015, 6, 5, 0, 0)'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'node': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['actors.Node']", 'related_name': "'sensor_reading'"}),
+            'sensor': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['actors.Sensor']", 'related_name': "'sensor_reading'"}),
+            'seqno': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'tag': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'auto_now': 'True', 'default': 'datetime.datetime(2015, 6, 5, 0, 0)'}),
+            'value': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'actors.sensor': {
+            'Meta': {'object_name': 'Sensor'},
+            'data_api_name': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'data_length': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modality': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'product_model': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'sensing_interval': ('django.db.models.fields.PositiveIntegerField', [], {})
+        },
+        'actors.sensormap': {
+            'Meta': {'object_name': 'SensorMap'},
+            'bit_position': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
+            'conf_seq': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['actors.ConfigurationSequence']", 'related_name': "'sensor_map'"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'sensor': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['actors.Sensor']", 'related_name': "'sensor_map'"})
+        },
+        'actors.statistics': {
+            'Meta': {'object_name': 'Statistics'},
+            'data_api_name': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'data_length': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modality': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'sensing_interval': ('django.db.models.fields.PositiveIntegerField', [], {})
+        }
+    }
+
+    complete_apps = ['actors']
